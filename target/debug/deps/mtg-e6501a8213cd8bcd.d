d3cd659174a432bf80d4226b41c7022071d1f3c7
Tripp Whaley <twhaley215@gmail.com>
2021-04-17 01:29:44 -0400

    initial commit

---
+++ target/debug/deps/mtg-e6501a8213cd8bcd.d
@@ -0,0 +1,5 @@
+/workspaces/mtg/target/debug/deps/mtg-e6501a8213cd8bcd: src/main.rs
+
+/workspaces/mtg/target/debug/deps/mtg-e6501a8213cd8bcd.d: src/main.rs
+
+src/main.rs:
---
+++ target/debug/mtg.d
@@ -0,0 +1,1 @@
+/workspaces/mtg/target/debug/mtg: /workspaces/mtg/src/main.rs
