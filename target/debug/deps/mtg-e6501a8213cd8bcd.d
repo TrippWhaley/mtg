a81014471d2d83f9c5dd542d5a250c2a2e233325
Tripp Whaley <twhaley215@gmail.com>
2021-04-19 17:43:57 -0400

    Delete target directory

--- target/debug/deps/mtg-e6501a8213cd8bcd.d
+++
@@ -1,5 +0,0 @@
-/workspaces/mtg/target/debug/deps/mtg-e6501a8213cd8bcd: src/main.rs
-
-/workspaces/mtg/target/debug/deps/mtg-e6501a8213cd8bcd.d: src/main.rs
-
-src/main.rs:
--- target/debug/mtg.d
+++
@@ -1,1 +0,0 @@
-/workspaces/mtg/target/debug/mtg: /workspaces/mtg/src/main.rs
